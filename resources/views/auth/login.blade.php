e03e3e658061b4abd922b9c19e3c61dcbcfb76fd
Dalitso Mandala <dalitso.mandala.prince@gmail.com>
2023-09-20 20:37:25 +0200

    added demo logins

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,7 @@
                 <div class="input-group flex-nowrap">
                     <span class="input-group-text" id="addon-wrapping"><i class="fa-regular fa-envelope"></i></span>
                     <x-text-input class="form-control form-icon-input" id="email" name="email" type="email"
-                        :value="old('email')" placeholder="name@gmail.com" required />
+                                  :value="old('email')" placeholder="name@gmail.com" required />
 
                     <!-- <span class="fas fa-user text-900 fs--1 form-icon"></span> Font Awesome fontawesome.com -->
                 </div>
@@ -34,7 +34,7 @@
             <div class="input-group flex-nowrap">
                 <span class="input-group-text" id="addon-wrapping"><i class="fa-solid fa-unlock"></i></span>
                 <x-text-input class="form-control form-icon-input" id="password" name="password" type="password"
-                    autocomplete="current-password" placeholder="password" required />
+                              autocomplete="current-password" placeholder="password" required />
 
                 <!-- <span class="fas fa-user text-900 fs--1 form-icon"></span> Font Awesome fontawesome.com -->
             </div>
@@ -64,46 +64,30 @@
         </div>
     </form>
 
-    {{-- <form method="POST" action="{{ route('login') }}">
-        @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+    <hr>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+    <h6 class="text-center text-uppercase">Demo</h6>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+    <div class="d-flex justify-content-around">
+        <button class="btn btn-phoenix-secondary me-1 mb-1 demo" data-selected="admin" type="button">Admin</button>
+        <button class="btn btn-phoenix-info me-1 mb-1 demo" data-selected="customer" type="button">Customer</button>
+    </div>
+    @push('scripts')
+        <script>
+            $(document).ready(function() {
+                $('.demo').click(function(e) {
+                    if ($(this).attr('data-selected') == 'admin') {
+                        $('#email').val('admin@gmail.com');
+                        $('#password').val('password');
+                    } else {
+                        $('#email').val('customer@gmail.com');
+                        $('#password').val('password');
+                    }
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+                });
+            });
+        </script>
+    @endpush
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form> --}}
 </x-guest-layout>
