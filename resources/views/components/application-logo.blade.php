637bb69fa47ab69922594dd79ba30449dfa16f7d
Dalitso Mandala <dalitso.mandala.prince@gmail.com>
2023-09-21 07:55:17 +0200

    fixed logo issue

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,8 @@
 <div class="d-flex align-items-center fw-bolder fs-5 d-inline-block"><img
-        src="@if (\App\Models\Company::find(1)->logo != null) {{ asset('storage/logo/' . \App\Models\Company::find(1)->logo) }}@else ' ' @endif"
-        alt="logo" {{ $attributes }}></div>
+         src="@if (\App\Models\Company::find(1) != null) @if (\App\Models\Company::find(1)->logo != null)
+
+         {{ asset('storage/logo/' . \App\Models\Company::find(1)->logo) }} @endif
+@else
+' ' @endif"
+
+         alt="logo" {{ $attributes }}></div>
