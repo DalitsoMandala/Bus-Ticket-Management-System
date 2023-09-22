be62c3a6f9e6ca8ca3d698fb70731ef5e38b62cf
Dalitso Mandala <dalitso.mandala.prince@gmail.com>
2023-09-22 07:35:47 +0200

    updated csrf

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
         </a>
     </div>
 
-    <div class="text-center mb-7 mt-2">
+    <div class="mt-2 text-center mb-7">
         <h3 class="text-1000">Sign Up</h3>
         <p class="text-700">Create an your account</p>
     </div>
@@ -18,16 +18,16 @@
         <div>
             <x-input-label for="name" :value="__('USERNAME')" />
             <x-text-input id="name" name="name" type="text" :value="old('name')" required autofocus
-                autocomplete="name" />
+                          autocomplete="name" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
         </div>
-        <div class="row mt-4">
+        <div class="mt-4 row">
 
             <div class="col">
                 <div>
                     <x-input-label for="name" :value="__('FIRST NAME')" />
                     <x-text-input id="first_name" name="first_name" type="text" :value="old('first_name')" required autofocus
-                        autocomplete="first_name" />
+                                  autocomplete="first_name" />
                     <x-input-error class="mt-2" :messages="$errors->get('first_name')" />
                 </div>
             </div>
@@ -36,7 +36,7 @@
                 <div>
                     <x-input-label for="name" :value="__('LAST NAME')" />
                     <x-text-input id="last_name" name="last_name" type="text" :value="old('last_name')" required autofocus
-                        autocomplete="last_name" />
+                                  autocomplete="last_name" />
                     <x-input-error class="mt-2" :messages="$errors->get('last_name')" />
                 </div>
             </div>
@@ -47,7 +47,7 @@
         <div class="mt-4">
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" name="email" type="email" :value="old('email')" required
-                autocomplete="username" />
+                          autocomplete="username" />
             <x-input-error class="mt-2" :messages="$errors->get('email')" />
         </div>
 
@@ -65,21 +65,21 @@
             <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
             <x-text-input id="password_confirmation" name="password_confirmation" type="password" required
-                autocomplete="new-password" />
+                          autocomplete="new-password" />
 
             <x-input-error class="mt-2" :messages="$errors->get('password_confirmation')" />
         </div>
 
 
-        <div class=" d-flex justify-content-end mb-7 mt-2">
+        <div class="mt-2  d-flex justify-content-end mb-7">
 
-            <a class="underline text-sm fs--1 text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                href="{{ route('login') }}">
+            <a class="text-sm text-gray-600 underline rounded-md fs--1 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+               href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
         </div>
 
-        <div class="d-flex  justify-content-center  mt-4">
+        <div class="mt-4 d-flex justify-content-center">
 
 
             <x-primary-button class="ml-4">
