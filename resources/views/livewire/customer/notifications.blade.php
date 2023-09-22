2566425a092afad398ac653dc6d45311cd8ec36e
Dalitso Mandala <dalitso.mandala.prince@gmail.com>
2023-09-22 12:43:23 +0200

    updated notifications

--- resources/views/livewire/customer/notifications.blade.php
+++ resources/views/livewire/customer/notifications.blade.php
@@ -20,64 +20,64 @@
 
 
     <a class="nav-link lh-1 pe-0 show" id="navbarDropdownUser" data-bs-toggle="dropdown" data-bs-auto-close="outside"
-        href="#!" role="button" aria-haspopup="true" aria-expanded="true" wire:click="readAll">
+       href="#!" role="button" aria-haspopup="true" aria-expanded="true" wire:click="readAll">
         <div>
 
             <span class="fa-regular @if (count($notifications) === 0) fa-bell-slash @else fa-bell @endif "
-                style="height:20px;width:20px;">{{ count($notifications) }}</span>
+                  style="height:20px;width:20px;">{{ count($notifications) }}</span>
 
 
             <span
-                class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger @if (count($notifications) === 0) d-none @endif">{{ count($notifications) }}</span>
+                  class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger @if (count($notifications) === 0) d-none @endif">{{ count($notifications) }}</span>
 
 
         </div>
     </a>
 
     <div class="py-0 border shadow dropdown-menu dropdown-menu-end navbar-dropdown-caret dropdown-profile border-300 "
-        data-bs-popper="static" aria-labelledby="navbarDropdownUser" wire:ignore.self>
+         data-bs-popper="static" aria-labelledby="navbarDropdownUser" wire:ignore.self>
         <div class="border-0 card position-relative">
             <div class="card-header">
                 <h5 class="fs-1">Notifications</h5>
             </div>
-            <div class="overflow-auto scrollbar" style="height: 15rem; width:400px;">
+            <div class="overflow-auto scrollbar" style="height: 15rem; ">
                 <ul class="pb-1 mb-2 nav d-flex flex-column" wire:ignore>
 
                     @if (count($notifications_data) > 0)
                         @foreach ($notifications_data as $notification)
                             <div
-                                class="px-2 py-3 px-sm-3 border-300 notification-card position-relative read border-bottom">
+                                 class="px-2 py-3 px-sm-3 border-300 notification-card position-relative read border-bottom">
                                 <div class="d-flex align-items-center justify-content-between position-relative">
                                     <div class="d-flex ">
                                         <a class="flex-shrink-0 avatar-xl me-3" href="{{ $notification->data['link'] }}"
-                                            style="text-decoration: none">
+                                           style="text-decoration: none">
                                             <span class="avatar-title bg-info-subtle text-info rounded-circle fs-16">
                                                 <i class="fa-solid fa-circle-exclamation" style="color:#ad81eb"></i>
                                             </span>
                                         </a>
                                         <a class="flex-1 me-sm-3" href="{{ $notification->data['link'] }}"
-                                            style="text-decoration: none">
+                                           style="text-decoration: none">
 
                                             <h4 class="text-black fs--1">{{ $notification->data['title'] }}</h4>
                                             <p
-                                                class="mb-2 fs--1 text-1000 mb-sm-3 fw-normal text-break line-clamp-2 lead">
+                                               class="mb-2 fs--1 text-1000 mb-sm-3 fw-normal text-break line-clamp-2 lead">
                                                 {!! $notification->data['description'] !!} <br><span
-                                                    class=" text-400 fw-bold fs--2">{{ \Carbon\Carbon::parse($notification->updated_at)->diffForHumans() }}</span>
+                                                      class=" text-400 fw-bold fs--2">{{ \Carbon\Carbon::parse($notification->updated_at)->diffForHumans() }}</span>
                                             </p>
                                             <p class="mb-0 text-800 fs--2"><span class="me-1 fas fa-clock"></span><span
-                                                    class="fw-bold">{{ \Carbon\Carbon::parse($notification->updated_at)->format('H:i A') }}
+                                                      class="fw-bold">{{ \Carbon\Carbon::parse($notification->updated_at)->format('H:i A') }}
                                                 </span>{{ \Carbon\Carbon::parse($notification->updated_at)->format('F j, Y') }}
                                             </p>
                                         </a>
                                     </div>
                                     <div class="font-sans-serif d-none"><button
-                                            class="transition-none btn fs--2 btn-sm dropdown-toggle dropdown-caret-none notification-dropdown-toggle"
-                                            data-stop-propagation="data-stop-propagation" data-bs-toggle="dropdown"
-                                            data-boundary="window" data-bs-reference="parent" type="button"
-                                            aria-haspopup="true" aria-expanded="false"><span
-                                                class="fas fa-ellipsis-h fs--2 text-900"></span></button>
+                                                class="transition-none btn fs--2 btn-sm dropdown-toggle dropdown-caret-none notification-dropdown-toggle"
+                                                data-stop-propagation="data-stop-propagation" data-bs-toggle="dropdown"
+                                                data-boundary="window" data-bs-reference="parent" type="button"
+                                                aria-haspopup="true" aria-expanded="false"><span
+                                                  class="fas fa-ellipsis-h fs--2 text-900"></span></button>
                                         <div class="py-2 dropdown-menu dropdown-menu-end"><a class="dropdown-item"
-                                                href="#!" wire:click="readData('{{ $notification->id }}')">Mark
+                                               href="#!" wire:click="readData('{{ $notification->id }}')">Mark
                                                 as
                                                 read</a></div>
                                     </div>
@@ -105,13 +105,12 @@
             </div>
             <div class="p-0 card-footer border-top">
                 <div
-                    class="my-2 text-center fw-bold fs--2 text-600 d-flex justify-content-between align-items-center ms-2 me-2">
+                     class="my-2 text-center fw-bold fs--2 text-600 d-flex justify-content-between align-items-center ms-2 me-2">
                     <a class="btn btn-phoenix-secondary "
-                        href="{{ route('customer-notification-history') }}">Notification
-                        history <i class="fas fa-arrow-up "></i></a>
+                       href="{{ route('customer-notification-history') }}">History <i class="fas fa-arrow-up "></i></a>
 
                     <a class="btn btn-soft-success fs--1 fw-normal" data-bs-toggle="button" href="#!"
-                        role="button" wire:click="readAll"><i class="fa-solid fa-check-double"></i> Mark
+                       role="button" wire:click="readAll"><i class="fa-solid fa-check-double"></i> Mark
                         all as
                         read</a>
                 </div>
