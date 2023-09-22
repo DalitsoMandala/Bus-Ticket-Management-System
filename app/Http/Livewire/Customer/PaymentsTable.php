db42c20ae6a55d8ee5271842a244b25408f683cb
Dalitso Mandala <dalitso.mandala.prince@gmail.com>
2023-09-22 07:46:01 +0200

    updated table

--- app/Http/Livewire/Customer/PaymentsTable.php
+++ app/Http/Livewire/Customer/PaymentsTable.php
@@ -64,7 +64,9 @@ final class PaymentsTable extends PowerGridComponent
         $data = Payment::query()->where('customer_id', auth()->user()->customers->first()->id)->select([
             'payments.*',
             DB::Raw('ROW_NUMBER() OVER (ORDER BY payments.id) AS rn'),
-        ])->get();
+        ])
+            ->orderBy('payments.id')
+            ->get();
 
         foreach ($data as $key => $paymentvalues) {
             # code...
